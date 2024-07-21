5f97bdf34c04072bd5036d60cabd976d06aceaa6
Edun Omatseye <edunomatseye@yahoo.com>
2024-07-21 20:51:25 +0100

    chore(lib) library initialization of products, orders and shared-ui react lib

---
+++ libs/orders/src/lib/orders.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledOrders = styled.div`
+  color: pink;
+`;
+
+export function Orders() {
+  return (
+    <StyledOrders>
+      <h1>Welcome to Orders!</h1>
+    </StyledOrders>
+  );
+}
+
+export default Orders;
